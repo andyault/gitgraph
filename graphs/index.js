308c9018a58bc82e0174ccd31e27ca5d6ef35b14
Andrew Ault <aault@linq.com>
2020-01-16 16:33:01 -0500

    make graphs/index automatic

--- graphs/index.js
+++ graphs/index.js
@@ -1,3 +1,12 @@
-exports['submitting-to-foh'] = require('./submitting-to-foh.js');
-exports['submitting-to-staging'] = require('./submitting-to-staging.js');
-exports['submitting-a-hotfix'] = require('./submitting-a-hotfix.js');
+//for each file in the graphs directory, require and map to exports
+const fs = require('fs');
+const path = require('path');
+
+const indexFilename = path.basename(__filename);
+const siblings = fs.readdirSync(__dirname);
+
+for (let i = 0; i < siblings.length; i++) {
+  const { base, name } = path.parse(siblings[i]);
+
+  if (base !== indexFilename) module.exports[name] = require('./' + base);
+}
