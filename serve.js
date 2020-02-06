ef753cc0fcba5c5174fc93070dca2534c8205874
Andrew Ault <aault@linq.com>
2020-02-06 15:18:32 -0500

    added index page

--- serve.js
+++ serve.js
@@ -10,6 +10,47 @@ const app = express();
 app.disable('etag');
 
 //
+app.get('/:type(|svg|img)', (req, res) => {
+  const { type } = req.params;
+
+  const links = Object.keys(graphs)
+    .map(name => {
+      const label = name.replace(/\-/g, ' ');
+
+      let link = `<li><a href="/${type ? type + '/' : ''}${name}">${label}</a>`;
+
+      if (!type) {
+        link += ` (<a href="/img/${name}">Img</a>)`;
+      }
+
+      link += `</li>`;
+      return link;
+    })
+    .join('\n');
+
+  let html = `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf8" />
+    </head>
+    <body>`;
+
+  if (!type) {
+    html += `
+      <div>Filter: <a href="/svg">Svg</a> <a href="/img">Img</a></div>`;
+  }
+
+  html += `
+      <ul style="text-transform: capitalize;">
+        ${links}
+      </ul>
+    </body>
+  </html>`;
+
+  res.send(html);
+});
+
 app.get('/:graph', (req, res) => res.redirect(`/svg/${req.params.graph}`));
 
 app.get('/:type(svg|img)/:graph', async (req, res, next) => {
